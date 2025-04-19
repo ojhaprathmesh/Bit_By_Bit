435c317f26a7bef82688b83330188daa0e693cdb
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2025-04-19 17:57:09 +0530

    Updates!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import Homepage from "./Pages/HomePage";
-import BitByBit from "./Pages/BitByBit.jsx";
+import BitByBit from "./Pages/BitByBit";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Loader from "./Loader/Loader.jsx";
@@ -20,9 +19,7 @@ function App() {
         <Loader />
       ) : (
         <Routes>
-          <Route element={<Homepage />} path="/" />
-          <Route element={<Navigate to="/bit-by-bit" replace />} path="/clue-hunt" />
-          <Route element={<BitByBit />} path="/bit-by-bit" />
+          <Route element={<BitByBit />} path="/" />
         </Routes>
       )}
     </Router>
